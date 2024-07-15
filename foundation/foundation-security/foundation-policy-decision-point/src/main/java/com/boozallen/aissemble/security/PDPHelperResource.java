471210229a0e3df49539d6459909ba21a7186909
Ryan Ashcraft <ryan.ashcraft@codifyiq.com>
2024-07-15 10:39:48 -0400

    - refactored extensions-authzforce  - refactored extensions-authzforce-attribute  - extensions-policy-decision-point-service  - updated velocity templates for generated files.

--- foundation/foundation-security/foundation-policy-decision-point/src/main/java/com/boozallen/aissemble/security/PDPHelperResource.java
+++ foundation/foundation-security/foundation-policy-decision-point/src/main/java/com/boozallen/aissemble/security/PDPHelperResource.java
@@ -1,8 +1,8 @@
-package com.boozallen;
+package com.boozallen.aissemble.security;
 
 /*-
  * #%L
- * aiSSEMBLE::Extensions::Security::Policy Decision Point Service
+ * AIOps Foundation::AIOps Core Security::AIOps Policy Decision Point
  * %%
  * Copyright (C) 2021 Booz Allen
  * %%
@@ -13,12 +13,11 @@ package com.boozallen;
 import com.boozallen.aissemble.security.authorization.models.AuthRequest;
 import com.boozallen.aissemble.security.authorization.models.PDPRequest;
 import com.boozallen.aissemble.security.authorization.policy.PolicyDecision;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-
 import com.boozallen.aissemble.security.authorization.AissembleSecureTokenServiceClient;
 import com.boozallen.aissemble.security.authorization.AissembleSimpleSecureTokenServiceClient;
 import com.boozallen.aissemble.security.authorization.policy.PolicyDecisionPoint;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -31,17 +30,11 @@ public class PDPHelperResource {
     private AissembleSecureTokenServiceClient tokenClient = new AissembleSimpleSecureTokenServiceClient();
     private PolicyDecisionPoint pdp = PolicyDecisionPoint.getInstance();
 
-    /**
-     * This REST resource checks with the security policies and determines if the provided
-     * identity is authorized to perform the requested action.
-     * @param pdpRequest The request which includes a subject, resource and action
-     * @return a policy decision
-     */
     @POST
     @Path("/pdp")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public String getPdpDecision(PDPRequest pdpRequest) {
+    public String gatPdpDecision(PDPRequest pdpRequest) {
         Jws<Claims> jws = tokenClient.parseToken(pdpRequest.getJwt());
         Claims claims = jws.getBody();
 
@@ -50,18 +43,13 @@ public class PDPHelperResource {
         return policyDecision.toString();
     }
 
-    /**
-     * This method authenticates the provide user
-     * @param authRequest the authentication request including a username and a password
-     * @return a valid jwt token if the user is successfully authenticated
-     */
     @POST
     @Path("/authenticate")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     public String authenticate(AuthRequest authRequest) {
-        AissembleSecureTokenServiceClient aissembleSecureTokenServiceClient = new AissembleSimpleSecureTokenServiceClient();
-        String jwt = aissembleSecureTokenServiceClient.authenticate(authRequest.getUsername(), authRequest.getPassword());
+        AissembleSecureTokenServiceClient aiopsSecureTokenServiceClient = new AissembleSimpleSecureTokenServiceClient();
+        String jwt = aiopsSecureTokenServiceClient.authenticate(authRequest.getUsername(), authRequest.getPassword());
 
         return jwt;
     }
