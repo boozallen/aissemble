26653f5075b47adb4fe47de2f47055b9d5027698
APE Gary <107056393+aaron-gary@users.noreply.github.com>
2024-08-15 13:31:08 -0700

    248 - fix build script so it runs integration tests (#274)

--- test/test-mda-models/aissemble-test-data-delivery-pyspark-model/tests/resources/integration-test-resources/docker-compose-setup.sh
+++ test/test-mda-models/aissemble-test-data-delivery-pyspark-model/tests/resources/integration-test-resources/docker-compose-setup.sh
@@ -21,8 +21,8 @@ then
   DOCKER_COMPOSE_RELEASE_URL="https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-${SYS_PLATFORM}-${SYS_ARCH}"
   echo "Installing docker-compose v2.12.2 (${SYS_PLATFORM} ${SYS_ARCH})"
   echo "Pulling binary from ${DOCKER_COMPOSE_RELEASE_URL}..."
-  curl -SL "${DOCKER_COMPOSE_RELEASE_URL}" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  sudo curl -SL "${DOCKER_COMPOSE_RELEASE_URL}" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 else
   echo "Already installed"
 fi
