1ef7d4a8059ccdb080270b8a06be9d5fa5194511
Chaoyi Sun <csun@cpointe-inc.com>
2024-08-16 10:46:30 -0700

    #266 config store property value injection

--- foundation/foundation-configuration-store/src/test/java/com/boozallen/aissemble/configuration/mutating/webhook/MutatingWebhookSteps.java
+++ foundation/foundation-configuration-store/src/test/java/com/boozallen/aissemble/configuration/mutating/webhook/MutatingWebhookSteps.java
@@ -12,30 +12,59 @@ package com.boozallen.aissemble.configuration.mutating.webhook;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.GroupVersionKind;
 import io.fabric8.kubernetes.api.model.GroupVersionResource;
-import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.admission.v1.AdmissionRequest;
 import io.fabric8.kubernetes.api.model.admission.v1.AdmissionReview;
 import io.fabric8.kubernetes.api.model.admission.v1.AdmissionReviewBuilder;
 import io.restassured.response.ValidatableResponse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MutatingWebhookSteps {
+    private static final String expectedInjectedValue = "env-access-key-id";
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private ValidatableResponse response;
     private AdmissionReview admissionReviewRequest;
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private ConfigMap configMap;
+    private Map<String, String > configMapData;
+    private String responsePatch;
 
-    @When("a kubernetes resource request is made")
+    @Given("a ConfigMap definition that contains the substitution key exists")
+    public void aConfigMapDefinitionThatContainsTheSubstituionKeyExists() {
+        final String propertyKey = "groupName=aws-credentials;propertyName=AWS_ACCESS_KEY_ID";
+        configMapData = new HashMap<>();
+        configMapData.put("AWS_ACCESS_KEY_ID", ConfigMutatingWebhook.CONFIG_STORE_INJECT_START + propertyKey + ConfigMutatingWebhook.CONFIG_STORE_INJECT_END);
+    }
+
+    @Given("the ConfigMap definition has the injection metadata label")
+    public void theConfigMapDefinitionHasTheInjectionMetatdataLabel() {
+        HashMap<String, String> metaLabels = new HashMap();
+        metaLabels.put("aissemble-configuration-store", "enabled");
+        ObjectMeta objectMeta = new ObjectMeta();
+        objectMeta.setLabels(metaLabels);
+        configMap = new ConfigMapBuilder()
+                .withData(configMapData)
+                .withMetadata(objectMeta)
+                .build();
+    }
+
+    @When("a kubernetes resource request is made to create a ConfigMap")
     public void aKubernetesResourceRequestIsMade() throws JsonProcessingException {
         this.admissionReviewRequest = createAdmissionReviewRequest();
 
@@ -50,46 +79,44 @@ public class MutatingWebhookSteps {
                 .then();
     }
 
-    @Then("the processed kubernetes resource is returned")
-    public void theProcessedKubernetesResourceIsReturned() throws JsonMappingException, JsonProcessingException {
+    @Then("the ConfigMap patch is returned")
+    public void theProcessedKubernetesResourceIsReturned() throws JsonProcessingException {
         response.statusCode(200);
 
         // Read in the response and verify it has the correct attributes
         AdmissionReview admissionReviewResponse = this.objectMapper.readValue(response.extract().response().asString(), AdmissionReview.class);
-
         assertTrue("The response should have allowed set to true", admissionReviewResponse.getResponse().getAllowed());
         assertEquals("The response uid did not match the request uid", this.admissionReviewRequest.getRequest().getUid(), admissionReviewResponse.getResponse().getUid());
         assertEquals("The response api version did not match the request api version", this.admissionReviewRequest.getApiVersion(), admissionReviewResponse.getApiVersion());
+
+        responsePatch = admissionReviewResponse.getResponse().getPatch();
+        assertNotNull(responsePatch);
     }
- 
+
+    @Then("the ConfigMap patch contains the injected value")
+    public void theConfigMapDefinitionContainsTheInjectedValue() throws JsonProcessingException {
+        JsonNode jsonPatch = objectMapper.readTree(new String(Base64.getDecoder().decode(responsePatch)));
+        assertEquals("The response patch does not contain the injected value", expectedInjectedValue, jsonPatch.findValue("value").asText());
+    }
     /**
      * Create an {@link AdmissionReviewRequest} for testing
      */
     private AdmissionReview createAdmissionReviewRequest() {
         AdmissionRequest request = new AdmissionRequest();
         request.setUid("example-uid");
-        request.setKind(new GroupVersionKind("", "Pod", "v1"));
-        request.setResource(new GroupVersionResource("", "pods", "v1"));
+        request.setKind(new GroupVersionKind("", "ConfigMap", "v1"));
+        request.setResource(new GroupVersionResource("", "configmaps", "v1"));
         request.setName("example-pod");
         request.setNamespace("default");
 
-        // Create a Pod object
-        Pod pod = new PodBuilder()
-            .withApiVersion("v1")
-            .withKind("Pod")
-            .withMetadata(new ObjectMetaBuilder().withName("example-pod").build())
-            .build();
-
         // Set the pod on the request
-        request.setObject(pod);
+        request.setObject(configMap);
 
         // Create AdmissionReview with the request
-        AdmissionReview admissionReviewRequest = new AdmissionReviewBuilder()
+        return new AdmissionReviewBuilder()
             .withApiVersion("admission.k8s.io/v1")
             .withKind("AdmissionReview")
             .withRequest(request)
             .build();
-
-        return admissionReviewRequest;
     }
 }
