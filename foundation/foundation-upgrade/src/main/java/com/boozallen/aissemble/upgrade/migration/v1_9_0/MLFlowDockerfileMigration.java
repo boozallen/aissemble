cb013b6be57d9b8c37338193a9acddba2162478e
Ryan Ashcraft <ryan.ashcraft@codifyiq.com>
2024-08-23 11:55:41 -0400

    #286 :bug: fix overzealous Dockerfile migration that was removing ARGs in all Dockerfiles

--- foundation/foundation-upgrade/src/main/java/com/boozallen/aissemble/upgrade/migration/v1_9_0/MLFlowDockerfileMigration.java
+++ foundation/foundation-upgrade/src/main/java/com/boozallen/aissemble/upgrade/migration/v1_9_0/MLFlowDockerfileMigration.java
@@ -17,26 +17,27 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.technologybrewery.baton.util.FileUtils.readAllFileLines;
 import static org.technologybrewery.baton.util.FileUtils.replaceLiteralInFile;
+import static org.technologybrewery.baton.util.FileUtils.writeFile;
 
 /**
-    Baton migration class to identify whether MLFlow's Dockerfile needs migration or not. If it does, then it will migrate
-    the Dockerfile to reference the community MLFlow docker image.
+ * Baton migration class to identify whether MLFlow's Dockerfile needs migration or not. If it does, then it will migrate
+ * the Dockerfile to reference the community MLFlow docker image.
  **/
 
 public class MLFlowDockerfileMigration extends AbstractAissembleMigration {
 
     public static final Logger logger = LoggerFactory.getLogger(MLFlowDockerfileMigration.class);
-    public static final Map<String, String> DOCKER_IMAGE_REFERENCE_MAP = Map.of(
-            "FROM ${DOCKER_BASELINE_REPO_ID}boozallen/aissemble-mlflow:1.7.0", "FROM bitnami/mlflow:2.15.1-debian-12-r0",
-            "ARG DOCKER_BASELINE_REPO_ID","",
-            "ARG VERSION_AISSEMBLE",""
-    );
+
+    public static final String FROM_BOOZALLEN_AISSEMBLE_MLFLOW_DOCKERFILE = "FROM ${DOCKER_BASELINE_REPO_ID}boozallen/aissemble-mlflow:1.7.0";
 
     /**
      * Function to check whether the migration is necessary.
+     *
      * @param file file to check
      * @return shouldExecute - whether the migration is necessary.
      */
@@ -45,11 +46,10 @@ public class MLFlowDockerfileMigration extends AbstractAissembleMigration {
         boolean shouldExecute = false;
         try (BufferedReader dockerfileReferenceConfig = new BufferedReader((new FileReader(file)))) {
             String line;
-            while((line = dockerfileReferenceConfig.readLine()) !=null && !shouldExecute) {
-                for (String key : DOCKER_IMAGE_REFERENCE_MAP.keySet()) {
-                    if (line.contains(key)) {
-                        shouldExecute = true;
-                    }
+            while ((line = dockerfileReferenceConfig.readLine()) != null) {
+                if (line.contains(FROM_BOOZALLEN_AISSEMBLE_MLFLOW_DOCKERFILE)) {
+                    shouldExecute = true;
+                    break;
                 }
             }
         } catch (Exception e) {
@@ -60,6 +60,7 @@ public class MLFlowDockerfileMigration extends AbstractAissembleMigration {
 
     /**
      * Performs the migration if the shouldExecuteOnFile() returns true.
+     *
      * @param file file to migrate
      * @return isMigrated - Whether the file was migrated successfully.
      */
@@ -68,10 +69,22 @@ public class MLFlowDockerfileMigration extends AbstractAissembleMigration {
         boolean isMigrated = false;
 
         try {
-            for (Map.Entry<String, String> entry : DOCKER_IMAGE_REFERENCE_MAP.entrySet()) {
-                replaceLiteralInFile(file, entry.getKey(), entry.getValue());
-                isMigrated = true;
+            // update FROM to point to community docker image:
+            replaceLiteralInFile(file, FROM_BOOZALLEN_AISSEMBLE_MLFLOW_DOCKERFILE, "FROM bitnami/mlflow:2.15.1-debian-12-r0");
+
+            // remove ARG that are no longer needed:
+            List<String> removalTargets = new ArrayList<>();
+            List<String> lines = readAllFileLines(file);
+            for (String line : lines) {
+                if ("ARG DOCKER_BASELINE_REPO_ID".equals(line) || "ARG VERSION_AISSEMBLE".equals(line)) {
+                    removalTargets.add(line);
+                }
             }
+            lines.removeAll(removalTargets);
+            writeFile(file, lines);
+
+            isMigrated = true;
+
         } catch (Exception e) {
             logger.error("Error in performing the migration for a refactored java package.");
         }
