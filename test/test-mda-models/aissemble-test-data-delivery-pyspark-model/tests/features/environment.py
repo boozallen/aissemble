2a6abc84abbae44d27025e1b36347d682fee1787
Choi, James [USA] <Choi_James@bah.com>
2024-10-24 16:38:39 -0400

    test-fix

--- test/test-mda-models/aissemble-test-data-delivery-pyspark-model/tests/features/environment.py
+++ test/test-mda-models/aissemble-test-data-delivery-pyspark-model/tests/features/environment.py
@@ -10,9 +10,13 @@
 import os
 import platform
 import json
+import packaging.version
+from container.safe_docker_container import SafeDockerContainer
+from importlib import metadata
 from pyspark.sql import SparkSession
 from krausening.logging import LogManager
 from testcontainers.compose import DockerCompose
+from testcontainers.core.waiting_utils import wait_for
 from aissemble_test_data_delivery_pyspark_model.generated import environment_base
 
 """
@@ -60,50 +64,70 @@ def before_scenario(context, scenario):
 def after_scenario(context, scenario):
     pass
 
+def start_container(context, docker_image, feature):
+    logger.info(f"Starting container: {docker_image}")
+    context.test_container.with_bind_ports(4566, 4566)
+    context.test_container.start()
+    wait_for_docker_url = "http://localhost:4566"
+    logger.info(
+        f"Waiting for Docker Compose services to start - waiting for a response from {wait_for_docker_url}"
+    )
+    wait_for(wait_for_docker_url)
+
 
 def before_feature(context, feature):
     if "integration" in feature.tags:
-        test_staging_path = "target/generated-sources/docker-compose-files"
-        logger.info(
-            f"Starting services defined in Docker Compose file at {test_staging_path}"
-        )
+        logger.info("Starting Test container services")
+        #piggyback test docker image
+        docker_image = "ghcr.io/boozallen/aissemble-vault:"
 
-        compose = DockerCompose(test_staging_path, "docker-compose.yml")
-        context.docker_compose_containers = compose
+        # append current version to docker image
+        # pyproject.toml has a "version" property, e.g. version = "0.12.0.dev"
+        # using major, minor, patch and -SNAPSHOT if dev
+        version = metadata.version("aissemble-extensions-encryption-vault-python")
+        docker_image += version_to_tag(version)
 
-        compose.start()
-        wait_for_docker_url = "http://localhost:4566"
-        logger.info(
-            f"Waiting for Docker Compose services to start - waiting for a response from {wait_for_docker_url}"
-        )
-        compose.wait_for(wait_for_docker_url)
+        context.test_container = SafeDockerContainer(docker_image)
+        start_container(context, docker_image, feature)
 
-        root_key_tuple = context.docker_compose_containers.exec_in_container(
-            ["cat", "/root_key.txt"], "vault"
-        )
-        secrets_root_key = root_key_tuple[0]
+        root_key_tuple = context.test_container.exec("cat /root_key.txt")
+        secrets_root_key = root_key_tuple.output.decode()
         os.environ["SECRETS_ROOT_KEY"] = secrets_root_key
 
-        unseal_keys_tuple = context.docker_compose_containers.exec_in_container(
-            ["cat", "/unseal_keys.txt"], "vault"
-        )
-        unseal_keys_txt = unseal_keys_tuple[0]
+        unseal_keys_tuple = context.test_container.exec("cat /unseal_keys.txt")
+        unseal_keys_txt = unseal_keys_tuple.output.decode()
         unseal_keys_json = json.loads(unseal_keys_txt)
         secrets_unseal_keys = ",".join(unseal_keys_json)
         os.environ["SECRETS_UNSEAL_KEYS"] = secrets_unseal_keys
 
-        transit_client_token_tuple = (
-            context.docker_compose_containers.exec_in_container(
-                ["cat", "/transit_client_token.txt"], "vault"
-            )
+        transit_client_token_tuple = context.test_container.exec(
+            "cat /transit_client_token.txt"
         )
-        transit_client_token_txt = transit_client_token_tuple[0]
+        transit_client_token_txt = transit_client_token_tuple.output.decode()
         transit_client_token_json = json.loads(transit_client_token_txt)
         encrypt_client_token = transit_client_token_json["auth"]["client_token"]
         os.environ["ENCRYPT_CLIENT_TOKEN"] = encrypt_client_token
 
 
+
 def after_feature(context, feature):
     if "integration" in feature.tags:
         logger.info("Stopping Docker Compose services")
-        context.docker_compose_containers.stop()
+        context.test_container.stop()
+
+
+def version_to_tag(version_str: str) -> str:
+    """Convert a python version into a docker tag for the same version.
+
+    Args:
+        version_str (str): The version string to convert.
+    Returns:
+        str: The docker tag for the version.
+    """
+    version = packaging.version.parse(version_str)
+    tag = version.base_version
+    if version.pre:
+        tag += "-" + "".join([str(x) for x in version.pre])
+    if version.is_devrelease:
+        tag += "-SNAPSHOT"
+    return tag
