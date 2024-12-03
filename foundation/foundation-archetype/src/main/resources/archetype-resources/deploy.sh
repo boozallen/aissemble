33b597a0ad7911adc1cdba34e4dc74b67d889044
Chaoyi Sun <csun@cpointe-inc.com>
2024-12-03 13:28:18 -0800

    #483 fix argocd deploy script

--- foundation/foundation-archetype/src/main/resources/archetype-resources/deploy.sh
+++ foundation/foundation-archetype/src/main/resources/archetype-resources/deploy.sh
@@ -1,19 +1,49 @@
 #!/bin/sh
 
-## ArgoCD local deployment script
-deploy() {
-  argocd app get ${artifactId} --server localhost:30080 --plaintext &> /dev/null
+APP_NAME=${artifactId}
+INFRA_NAME=infrastructure
 
-  if [ $? -eq 0 ]; then
+print_usage() {
+  echo "Usage: $0 [up|down|shutdown]"
+  echo "   up          deploy application (automatically starts deployment infrastructure if needed)"
+  echo "   down        tear down application"
+  echo "   shutdown    tear down deployment infrastructure"
+}
+
+startup() {
+  echo "Checking for deployment infrastructure..."
+  helm status $INFRA_NAME > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    helm upgrade --install $INFRA_NAME ${artifactId}-infrastructure \
+      --values ${artifactId}-infrastructure/values.yaml \
+      --values ${artifactId}-infrastructure/values-dev.yaml
+    if ! kubectl wait -A --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server; then
+      exit $?
+    fi
+    argocd repo add ${projectGitUrl} --server localhost:30080 --plaintext --insecure-skip-server-verification
+  fi
+}
+
+is_app_running() {
+  argocd app get $APP_NAME --server localhost:30080 --plaintext > /dev/null 2>&1
+}
+
+deploy() {
+  startup
+
+  if is_app_running; then
     echo "${artifactId} is deployed"
   else
-    argocd app create ${artifactId} \
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    echo "Deploying ${artifactId} from branch '$branch'..."
+    argocd app create $APP_NAME \
       --server localhost:30080 --plaintext \
       --dest-namespace ${artifactId} \
       --dest-server https://kubernetes.default.svc \
       --repo ${projectGitUrl} \
       --path ${artifactId}-deploy/src/main/resources \
-      --revision main \
+      --revision $branch \
+      --helm-set spec.targetRevision=$branch \
       --values values.yaml \
       --values values-dev.yaml \
       --sync-policy automated
@@ -21,25 +51,34 @@ deploy() {
 }
 
 down() {
-  argocd app get ${artifactId} --server localhost:30080 --plaintext &> /dev/null
-
-  if [ $? -eq 0 ]; then
-    argocd app delete ${artifactId} --server localhost:30080 --plaintext
+  if is_app_running; then
+    echo "Tearing down app..."
+    argocd app delete $APP_NAME --server localhost:30080 --plaintext --yes
   else
     echo "${artifactId} is not deployed"
+  fi
 }
 
-print_usage() {
-  echo "Usage: $0 [up|down]"
+shutdown() {
+  helm status $INFRA_NAME > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Infrastructure already shutdown"
+  else
+    if is_app_running; then
+      down
+    fi
+    echo "Shutting down infrastructure..."
+    helm uninstall $INFRA_NAME
+  fi
 }
+
 
 if [ "$1" = "up" ]; then
   deploy
+elif [ "$1" = "down" ]; then
+  down
+elif [ "$1" = "shutdown" ]; then
+  shutdown
 else
-  if [ "$1" = "down" ]; then
-    down
-  else
-    print_usage
-  fi
+  print_usage
 fi
-
